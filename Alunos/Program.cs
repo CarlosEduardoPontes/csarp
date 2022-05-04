274fbc1a159ea990951ca4ad2e8dec116cf8896f
CarlosEduardoPontes <ceduardo.pontes@hotmail.com>
2022-05-04 15:42:15 -0300

    Desenvolvendo o Menu

--- Alunos/Program.cs
+++ Alunos/Program.cs
@@ -8,6 +8,69 @@ namespace Alunos
     {
         static void Main(string[] args)
         {
+            Aluno[] alunos = new Aluno[5];
+            var indiceAluno = 0;
+            string opcaoEscolhida = ObterOpcaoUsuario();
+
+            while(opcaoEscolhida.ToUpper() != "X")
+            {
+                switch (opcaoEscolhida)
+                {
+                    case "1":
+                        Console.WriteLine("Informe o nome do Aluno:");
+                        var aluno = new Aluno();
+                        aluno.Nome = Console.ReadLine();
+
+                        Console.WriteLine("Informe a nota desse Aluno:");
+
+                        if(decimal.TryParse(Console.ReadLine(), out decimal nota))
+                        {
+                            aluno.Nota = nota;
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Valor da nota deve ser numero decimal");
+                        }
+
+                        alunos[indiceAluno] = aluno;
+                        indiceAluno++;
+                        break;
+                    
+                    case "2":
+                        foreach(var a in alunos)
+                        {
+                            if(!string.IsNullOrEmpty(a.Nome))
+                            {
+                            Console.WriteLine($"ALUNO:{a.Nome} - NOTA {a.Nota} ");
+                            }
+                        }
+                        break;
+
+                    case "3":
+                        decimal notaTotal = 0;
+                        var nrAlunos = 0;
+
+                        for(int i = 0; i <alunos.Length; i++)
+                        {
+                            if(!string.IsNullOrEmpty(alunos[i].Nome))
+                            {
+                                notaTotal = notaTotal + alunos[i].Nota;
+                                nrAlunos++;
+                            }
+                        }
+                        var mediaGeral = notaTotal / nrAlunos;
+                        Console.WriteLine($"MEDIA GERAL: {mediaGeral}");
+
+                        break;
+                        default:
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoEscolhida = ObterOpcaoUsuario();
+            }
+        }
+        private static string ObterOpcaoUsuario()
+        {
             Console.WriteLine("");
             Console.WriteLine("Escolha a opção desejada: ");
             Console.WriteLine("1 - Inserir novo Aluno");
@@ -15,6 +78,10 @@ namespace Alunos
             Console.WriteLine("3 - Calcular média Geral");
             Console.WriteLine("X - Sair");
             Console.WriteLine("");
+
+            string opcaoEscolhida = Console.ReadLine();
+            Console.WriteLine("");
+            return opcaoEscolhida;
         }
     } 
 }
